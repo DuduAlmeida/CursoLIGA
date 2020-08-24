4a4c997ed49283b6005a9283022fa68b174897bf
Eduardo Almeida <contato.eduardopex@hotmail.com>
2020-08-24 18:05:30 -0300

    Includded the main page

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -7,9 +7,10 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  styleUrls: ['app.component.scss'],  
 })
 export class AppComponent {
+  
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
