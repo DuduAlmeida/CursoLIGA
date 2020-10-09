7427d3628bfc75947e5619794b9de0cfb2750f0f
Eduardo Almeida <contato.eduardopex@hotmail.com>
2020-10-09 17:17:51 -0300

    feat(create-comment): updated form

--- birdApp/src/app/pages/main/create-comment/create-comment.page.ts
+++ birdApp/src/app/pages/main/create-comment/create-comment.page.ts
@@ -1,6 +1,10 @@
+import { CommentService } from 'src/app/services/comment/comment.service';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AvatarItem } from 'src/app/models/interfaces/avatar-item';
+import { CreateCommentPayload } from 'src/app/models/payloads/create-comment.payload';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-create-comment',
@@ -12,6 +16,10 @@ export class CreateCommentPage implements OnInit {
   constructor(
     private readonly route: ActivatedRoute,
     private readonly router: Router,
+    private readonly fb: FormBuilder,
+    private readonly comment: CommentService,
+    private readonly toast: ToastController,
+
   ) {
 
     this.categoryId = Number(this.route.snapshot.paramMap.get('categoryId')) | 0;
@@ -19,7 +27,12 @@ export class CreateCommentPage implements OnInit {
     if (this.categoryId === 0)
       return void this.router.navigateByUrl('/main/categories');
 
-    console.log(this.categoryId);
+      this.formGroup = this.fb.group({
+        personName: ['', Validators.required],
+        personAvatarIndex: [0, Validators.required],
+        categoryId: [this.categoryId, Validators.required],
+        message: ['', Validators.required],
+      })
 
   }
 
@@ -34,6 +47,16 @@ export class CreateCommentPage implements OnInit {
   public categoryId: number;
 
   /*** 
+   * A referência do formulário
+   */
+  public formGroup: FormGroup;
+
+  /*** 
+   * Diz se você está enviando esse formulário
+   */
+  public isSendingForm: boolean = false;
+
+  /*** 
    * A lista de avatares disponíveis para o usuário
    */
   public readonly listAvatars: AvatarItem[] = [
@@ -44,4 +67,54 @@ export class CreateCommentPage implements OnInit {
   ]
   
   /* #Endregion Public Properties*/
+
+  /* #region Public Methods*/
+  
+  /*** 
+   * Método executado quando o formulário é enviado
+   */
+  public async onSubmit(){
+    if(this.isSendingForm)
+      return;
+    
+    this.isSendingForm = true;
+
+    const { personAvatarIndex, ...otherValues } = this.formGroup.getRawValue();
+
+    const payload: CreateCommentPayload = {
+      ...otherValues,
+      personEmoji: this.listAvatars[personAvatarIndex].personEmoji,
+      personColor: this.listAvatars[personAvatarIndex].personColor,
+    }
+    
+    const { isSuccess, result } = await this.comment.createComment(payload);
+
+    await this.showMessage(result);
+
+    if(!isSuccess)
+      return ;
+
+    await this.router.navigateByUrl(`/main/categories/${this.categoryId}`);
+    this.isSendingForm = false;
+  }
+  
+  /* #Endregion Public Methods*/
+
+  /* #region Private Methods*/
+  
+  /*** 
+   * Método que exibe a mensagem de erro
+   * 
+   * @param message A mensagem
+   */
+  private async showMessage(message: string): Promise<void>{
+    const toast = await this.toast.create({
+      message,
+      duration: 5_000,
+    });
+
+    await toast.present();
+  }
+  
+  /* #Endregion Private Methods*/
 }
