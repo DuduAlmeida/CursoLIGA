164e25221a98a33de926459d4d2b16f9e8a9185c
Eduardo Almeida <contato.eduardopex@hotmail.com>
2020-10-15 16:12:26 -0300

    refactor(environment): updated api params

--- birdApp/src/environments/environment.prod.ts
+++ birdApp/src/environments/environment.prod.ts
@@ -7,5 +7,19 @@ export const environment = {
   },
   keys: {
     myComments: 'MY_COMMENTS_KEY',
-  }
+  },
+  api: {
+    baseUrl: 'http://localhost:3000',
+    comment: {
+      list: '/comments',
+      listPaginated: '/comments?page={currentPage}&maxItens={maxItens}&includeCategory=true',
+      listPaginatedById: '/comments?page={currentPage}&maxItens={maxItens}&categoryId={categoryId}&includeCategory=true',
+      create: '/comments'
+    },
+    category: {
+      list: '/categories',
+      listPaginated: '/categories?page={currentPage}&maxItens={maxItens}',
+      create: '/categories'
+    },
+  },
 };
